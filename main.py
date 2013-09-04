286662c1982bd10045e7cbc2b33331736e2eed09
Brian Gillespie <bn_gillespie@hotmail.com>
2013-09-03 23:57:45 -0400

    second commit

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from Engine import *
 from sys import exit
 
+#Game continues playing until the user tells it to stop
 stopPlaying = False
 while not stopPlaying:
 	
+    #User determines number of computer players to play against
 	num_players = int(raw_input("How many players do you want to play against?\n> "))
 	
+    #Cannot deal more cards than are in the deck
 	if num_players > 9:
 		print "Too many players! You'll use up the whole deck."
 		print "Goodbye!"
 		exit(1)
 	
+    #Play game
 	game = Engine(num_players)
 	print game.play()
-		
+	
+    #User determines if (s)he wants to continue playing
 	next = raw_input("Keep playing?(y/n)\n> ")
 	
 	if 'n' in next:
